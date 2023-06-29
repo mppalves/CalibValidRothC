c9206d151e5595c6cfa5a12caae7dbb7ef90c16f
Andrew Manderson <a.manderson@live.co.uk>
2023-06-29 10:53:50 +0100

    Use the T-distribution to construct prediction intervals, as n is small.
    Also compute the (integer) number of fields that would additionally need to be within an prediction interval to achieve nominal coverage.

--- R/conf_int.R
+++ R/conf_int.R
@@ -18,16 +18,24 @@ conf_int <- function(data_out) {
     dplyr::group_by(Practice_Category, CFGs, Climate_zones_IPCC) %>%
     dplyr::summarise(sd = sd(model_error), n = dplyr::n(), .groups = "drop")
 
+  # 90% prediction interval, 5% either side, use t-dist as n is low and
+  # sd is estimated
   Ci <- dplyr::left_join(diffs, sds, by = c("Practice_Category", "CFGs", "Climate_zones_IPCC")) %>%
     dplyr::mutate(
-      cilower = observed_diff - 1.64 * sd,
-      ciupper = observed_diff + 1.64 * sd,
+      cilower = observed_diff + qt(p = 0.05, df = n) * sd,
+      ciupper = observed_diff + qt(p = 0.95, df = n) * sd,
       fill = ifelse(predicted_diff < cilower | predicted_diff > ciupper, "Outside C.I.", "Inside C.I.")
     )
 
   Ci_percent <- Ci %>%
     dplyr::group_by(Practice_Category, CFGs, Climate_zones_IPCC) %>%
-    dplyr::summarise(ci_percent = mean(fill == "Inside C.I.") * 100, .groups = "drop")
+    dplyr::summarise(
+      ci_percent = mean(fill == "Inside C.I.") * 100,
+      fields_inside = sum(fill == "Inside C.I."),
+      total_fields = n(),
+      addtional_fields_to_90 = max(0, ceiling((0.9 * total_fields) - fields_inside)),
+      .groups = "drop"
+    )
 
   return(list("Ci_percent" = Ci_percent, "Ci" = Ci))
 }
