f35a1d430bd3249c2c226e71009f986c85ffab6f
Andrew Manderson <a.manderson@live.co.uk>
2023-07-03 11:32:07 +0100

    Mostly fix the use of `tryCatch` to handle malformed errors.
    - Todo: sure the error messages get output, but not in a perf-compromising way

--- R/optm_fun.R
+++ R/optm_fun.R
@@ -13,25 +13,43 @@ optm_fun <- function(params, train_data) {
   x_train <- train_data[["x_train"]]
   y_train <- train_data[["y_train"]]
   simulated_data <- rep(NA, nrow(x_train))
+  bad_field_ids <- list()
+  #TODO: Make sure actual error msgs get pushed to warnings
+  # put do not compromise perf.
+  #err_msgs <- list()
 
-  tryCatch(
-    expr = {
-      for (i in 1:nrow(x_train)) {
-        simulated_data[i] <- sum(tail(
+  for (ii in seq_len(nrow(x_train))) {
+    tryCatch(
+      expr = {
+        simulated_data[ii] <- sum(tail(
           AgreenaRothC2::ag_rothC(
-            x_train[i, ],
-            cf3 = params[1], cf4 = params[2], cf5 = params[3], write = FALSE
+            x_train[ii, ],
+            cf3 = params[1],
+            cf4 = params[2],
+            cf5 = params[3],
+            write = FALSE
           ),
           1 # tail
         ))
+      },
+      error = function(e) {
+        bad_field_ids[[ii]] <<- as.data.frame(x_train)[ii, "Field_ID"]
+        #err_msgs[[ii]] <- e
       }
-    },
-    error = function(e) {
-      message(paste("Field_ID:", x_train[i, "Field_ID"]))
-      print(e)
-    }
-  )
+    )
+  }
 
-  res <- sum((simulated_data - y_train)^2) / length(simulated_data)
+  if (length(bad_field_ids) != 0) {
+    out_warn_msg <- paste0(
+      "Errors from rows with Field_ID: ",
+      paste0(unlist(bad_field_ids), collapse = ", ")
+    )
+
+    warning(out_warn_msg)
+  }
+
+  not_na_indices <- !is.na(simulated_data)
+  n_not_na <- sum(not_na_indices == TRUE)
+  res <- sum((simulated_data[not_na_indices] - y_train[not_na_indices])^2) / n_not_na
   return(res)
 }
